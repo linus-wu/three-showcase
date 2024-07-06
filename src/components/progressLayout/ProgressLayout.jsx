58846eff5ba0ad96d4d6f01ebaa2b5157c546c9a
Linus <linus@LinusdeMacBook-Pro.local>
2024-07-07 00:28:25 +0800

    fix sidebar ui

--- src/components/progressLayout/ProgressLayout.jsx
+++ src/components/progressLayout/ProgressLayout.jsx
@@ -1,6 +1,6 @@
 const ProgressLayout = ({ loadingPercentage }) => {
   return (
-    <div className="absolute top-16 left-0 w-full h-full flex justify-center items-center bg-black z-50">
+    <div className="absolute top-16 left-0 w-full h-full flex justify-center items-center bg-black z-10">
       <div className="w-9/12">
         <div
           className="bg-blue-950 h-2 rounded-lg"
